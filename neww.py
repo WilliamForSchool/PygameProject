fd6cdcc97e4df1bd3f1a638ac1690a187524b54f
William Morris <WBMorris24@damien-hs.edu>
2024-02-21 10:00:48 -0800

    Update/Template for Pygame

--- neww.py
+++ neww.py
@@ -1,8 +1,9 @@
 import pygame
+from sys import exit 
 
 pygame.init() # initialize and start the pygame engine
 screen = pygame.display.set_mode((800, 600)) # open a window with size
-
+clock = pygame.time.Clock() # allows us to set FPS rates
 
 while True: 
 	for event in pygame.event.get():
@@ -10,3 +11,4 @@ while True:
 			pygame.quit()
 			exit()
 	pygame.display.update()
+	clock.tick(60)
